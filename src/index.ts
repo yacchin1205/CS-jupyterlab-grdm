3d7cb872ee6a54b7050f8e7a3d70ac98934ec04b
yacchin1205 <968739+yacchin1205@users.noreply.github.com>
2023-10-09 10:35:52 +0900

    Fix location of "Sync to GRDM" button

--- src/index.ts
+++ src/index.ts
@@ -2,10 +2,42 @@ import {
   JupyterFrontEnd,
   JupyterFrontEndPlugin
 } from '@jupyterlab/application';
+import { Toolbar } from '@jupyterlab/ui-components';
+import { Widget } from '@lumino/widgets';
 import { IDefaultFileBrowser } from '@jupyterlab/filebrowser';
 import { ITranslator } from '@jupyterlab/translation';
 
 import { createSyncButton } from './button';
+
+
+function insertItemBefore(
+  target: Toolbar<Widget>,
+  name: string,
+  item: Widget,
+  originClassName: string,
+  timeout: number,
+) {
+  if (timeout < 0) {
+    console.warn('Origin not found', originClassName);
+    target.addItem(name, item);
+    return;
+  }
+  const children = new Array(...target.children())
+    .map((element, index) => ({
+      element,
+      index,
+    }))
+    .filter(({ element }) => element.node.classList.contains(originClassName));
+  if (children.length > 0) {
+    const { index } = children[0];
+    target.insertItem(index, name, item);
+    return;
+  }
+  const nextTimeout = 100;
+  setTimeout(() => {
+    insertItemBefore(target, name, item, originClassName, timeout - nextTimeout);
+  }, nextTimeout)
+}
 
 /**
  * Initialization data for the rdm-binderhub-jlabextension extension.
@@ -25,7 +57,14 @@ const plugin: JupyterFrontEndPlugin<void> = {
     );
     const trans = translator.load('jupyterlab');
     const sync = createSyncButton(trans);
-    browser.toolbar.addItem('sync_to_grdm', sync);
+    /* "Sync to GRDM" button is placed to the left of the filter text box */
+    insertItemBefore(
+      browser.toolbar,
+      'sync_to_grdm',
+      sync,
+      'jp-FileBrowser-filterBox',
+      1000, /*timeout*/
+    );
   }
 };
 
